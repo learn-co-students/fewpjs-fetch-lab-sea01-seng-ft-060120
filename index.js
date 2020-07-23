8f85e2dee8325177076248f93bec643744e0accf
bbtrombone <bbtrombone@sbcglobal.net>
2020-07-23 09:42:59 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+ 
 }
 
 function renderBooks(books) {
@@ -14,3 +17,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
